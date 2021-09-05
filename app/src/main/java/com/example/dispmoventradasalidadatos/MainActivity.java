7aee70773652b918eae7925cd969bc4baa1ba737
Julian Lopez <jlopezsa@gmail.com>
2021-09-04 22:52:26 -0500

    Ejemplo de entrada y salida de un numero real

---
+++ app/src/main/java/com/example/dispmoventradasalidadatos/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.dispmoventradasalidadatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView salida;
+    private EditText operador1;
+    private Button boton;
+    private double numeroOperador;
+
+    private TextView mensaje_inferior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mensaje_inferior = (TextView) findViewById(R.id.textView);
+        mensaje_inferior.setText("Ejemplo de entrada y salida de datos");
+
+        salida = (TextView) findViewById(R.id.txtSalida);
+        operador1 = (EditText) findViewById(R.id.txtOperador1);
+        boton = (Button) findViewById(R.id.btnEnvio);
+
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeroOperador = Double.parseDouble(operador1.getText().toString());
+
+                salida.setText("El número ingresado es: " + numeroOperador);
+
+                Log.i("FLAG(MainActivity)", "El valor ingresado es: "+ numeroOperador); // Mensaje en Logcat
+            }
+        });
+
+        //salida.setText("Prueba desde codigo");
+
+
+    }
+}
